f531a87ae9323ecfc4af4787c4e658462798f916
elijah-kipsang <elijahkipsang@outlook.com>
2022-09-08 17:13:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Printing
  *
- * Return: Always 0 (Successful)
- */
+ * * main - Printing
+ *
+ * *
+ *
+ * * Return: Always 0 (Successful)
+ *
+ * */
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual
-puzzle");
+
+			puzzle");
+
 	return (0);
+
 }
