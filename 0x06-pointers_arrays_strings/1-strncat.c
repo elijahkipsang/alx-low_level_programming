c019c17cff7080acbec87cad8666efc864d54636
elijah-kipsang <elijahkipsang@outlook.com>
2022-09-21 12:31:06 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,6 +10,7 @@
 
 char *_strncat(char *dest, char *src, int n) 
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
@@ -19,4 +20,5 @@ char *_strncat(char *dest, char *src, int n)
 		dest[dest_len++] = src[index];
 
 	return (dest);
+
 }
